8aabcaeab8ccf6213d84cf5c2aab114a21bc847a
Lukas Burgey <ubedv@student.kit.edu>
2017-05-15 14:54:07 +0200

    Implement ssh key generation

--- main.go
+++ main.go
@@ -1,45 +1,81 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	l "git.scc.kit.edu/lukasburgey/wattsPluginLib"
+	"golang.org/x/crypto/ssh"
+	mrand "math/rand"
+	"time"
 )
 
-// do not return a plain output yourself.
-// use PluginGoodRequest, PluginError, PluginAdditionalLogin instead
+const (
+	letterBytes   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits
+)
+
+// http://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-golang
+func randStringBytesMaskImprSrc(n int) []byte {
+	var src = mrand.NewSource(time.Now().UnixNano())
+	b := make([]byte, n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return b
+}
+
+func generateKey() (privateKey string, publicKey string, password string) {
+	bits := 4096
+	rsaPrivateKey, err := rsa.GenerateKey(rand.Reader, bits)
+	l.Check(err, 1, "rsa private key generation")
+
+	pkcs1 := x509.MarshalPKCS1PrivateKey(rsaPrivateKey)
+	passwordBytes := randStringBytesMaskImprSrc(16)
+	encryptedPemBlock, err := x509.EncryptPEMBlock(
+		rand.Reader, "RSA PRIVAT KEY", pkcs1, passwordBytes, x509.PEMCipherAES256)
+	l.Check(err, 1, "encrypting pem block")
+
+	sshPublicKey, err := ssh.NewPublicKey(&rsaPrivateKey.PublicKey)
+	l.Check(err, 1, "ssh public key generation")
+
+	privateKey = string(pem.EncodeToMemory(encryptedPemBlock))
+	publicKey = string(ssh.MarshalAuthorizedKey(sshPublicKey))
+	password = string(passwordBytes)
+	return
+}
+
 func request(plugin l.Plugin) l.Output {
-	// exemplarily plugin input access
-	foo := plugin.PluginInput.AccessToken
+	if _, ok := plugin.PluginInput.Params["pub_key"]; ok {
+		return l.PluginError("provided key not implemented")
+	} else {
+		privateKey, publicKey, password := generateKey()
 
-	// exemplarily error
-	if foo == "bar" {
-		return l.PluginError("revoke failed")
+		credential := []l.Credential{
+			l.Credential{Name: "private key", Type: "string", Value: privateKey},
+			l.Credential{Name: "public key", Type: "string", Value: publicKey},
+			l.Credential{Name: "password", Type: "string", Value: password},
+		}
+		credentialState := "registerred"
+		return l.PluginGoodRequest(credential, credentialState)
 	}
-
-	// exemplarily additional login
-	additionalLoginRequirred := false
-	if additionalLoginRequirred {
-		return l.PluginAdditionalLogin("<provider id>", "<user msg>")
-	}
-
-	// do the request that yields a credential
-	credential := []l.Credential{
-		l.Credential{
-			Name:  "foo",
-			Type:  "string",
-			Value: "bar",
-		},
-	}
-	credentialState := "registerred"
-	return l.PluginGoodRequest(credential, credentialState)
 }
 
 func revoke(plugin l.Plugin) l.Output {
-	// exemplarily error
-	err := false
-	if err {
-		return l.PluginError("revoke failed")
-	}
-	return l.PluginGoodRevoke()
+	return l.PluginError("revoke not implemented")
 }
 
 func main() {
@@ -49,50 +85,18 @@ func main() {
 		ActionRequest: request,
 		ActionRevoke:  revoke,
 		ConfigParams: []l.ConfigParamsDescriptor{
-			l.ConfigParamsDescriptor{
-				Name:    "myproxy_server",
-				Type:    "string",
-				Default: "master.data.kit.edu",
-			},
-			l.ConfigParamsDescriptor{
-				Name:    "myproxy_server_pwd",
-				Type:    "string",
-				Default: "",
-			},
-			l.ConfigParamsDescriptor{
-				Name:    "script_path",
-				Type:    "string",
-				Default: "./ssh_trigger.py",
-			},
-			l.ConfigParamsDescriptor{
-				Name:    "remote_script",
-				Type:    "string",
-				Default: "./myproxy_ssh_vm.py",
-			},
-			l.ConfigParamsDescriptor{
-				Name:    "host",
-				Type:    "string",
-				Default: "watts-x509.data.kit.edu",
-			},
-			l.ConfigParamsDescriptor{
-				Name:    "user",
-				Type:    "string",
-				Default: "x509",
-			},
-			l.ConfigParamsDescriptor{
-				Name:    "work_dir",
-				Type:    "string",
-				Default: "/tmp",
-			},
+			l.ConfigParamsDescriptor{Name: "myproxy_server", Type: "string", Default: "master.data.kit.edu"},
+			l.ConfigParamsDescriptor{Name: "myproxy_server_pwd", Type: "string", Default: ""},
+			l.ConfigParamsDescriptor{Name: "script_path", Type: "string", Default: "./ssh_trigger.py"},
+			l.ConfigParamsDescriptor{Name: "remote_script", Type: "string", Default: "./myproxy_ssh_vm.py"},
+			l.ConfigParamsDescriptor{Name: "host", Type: "string", Default: "watts-x509.data.kit.edu"},
+			l.ConfigParamsDescriptor{Name: "user", Type: "string", Default: "x509"},
+			l.ConfigParamsDescriptor{Name: "work_dir", Type: "string", Default: "/tmp"},
 		},
 		RequestParams: []l.RequestParamsDescriptor{
 			l.RequestParamsDescriptor{
-				Key:         "pub_key",
-				Name:        "public key",
-				Description: "the public key of the service",
-				Type:        "textarea",
-				Mandatory:   false,
-			},
+				Key: "pub_key", Name: "public key", Description: "the public key of the service", Type: "textarea",
+				Mandatory: false},
 		},
 	}
 	l.PluginRun(pluginDescriptor)
