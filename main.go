75c77519b1dea81537c94dd0b2e1307979f9430d
Lukas Burgey <ubedv@student.kit.edu>
2017-05-15 18:21:14 +0200

    Implement request action

--- main.go
+++ main.go
@@ -1,21 +1,28 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
+	"encoding/json"
 	"encoding/pem"
+	"fmt"
 	l "git.scc.kit.edu/lukasburgey/wattsPluginLib"
+	"github.com/kalaspuffar/base64url"
 	"golang.org/x/crypto/ssh"
 	mrand "math/rand"
+	"os/exec"
 	"time"
 )
 
 const (
-	letterBytes   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	letterIdxBits = 6                    // 6 bits to represent a letter index
-	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
-	letterIdxMax  = 63 / letterIdxBits
+	letterBytes       = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	letterIdxBits     = 6                    // 6 bits to represent a letter index
+	letterIdxMask     = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax      = 63 / letterIdxBits
+	rsaBits           = 4096
+	rsaPasswordLength = 1
 )
 
 // http://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-golang
@@ -39,12 +46,11 @@ func randStringBytesMaskImprSrc(n int) []byte {
 }
 
 func generateKey() (privateKey string, publicKey string, password string) {
-	bits := 4096
-	rsaPrivateKey, err := rsa.GenerateKey(rand.Reader, bits)
+	rsaPrivateKey, err := rsa.GenerateKey(rand.Reader, rsaBits)
 	l.Check(err, 1, "rsa private key generation")
 
 	pkcs1 := x509.MarshalPKCS1PrivateKey(rsaPrivateKey)
-	passwordBytes := randStringBytesMaskImprSrc(16)
+	passwordBytes := randStringBytesMaskImprSrc(rsaPasswordLength)
 	encryptedPemBlock, err := x509.EncryptPEMBlock(
 		rand.Reader, "RSA PRIVAT KEY", pkcs1, passwordBytes, x509.PEMCipherAES256)
 	l.Check(err, 1, "encrypting pem block")
@@ -62,15 +68,70 @@ func request(plugin l.Plugin) l.Output {
 	if _, ok := plugin.PluginInput.Params["pub_key"]; ok {
 		return l.PluginError("provided key not implemented")
 	} else {
+		// generate a new key
 		privateKey, publicKey, password := generateKey()
 
-		credential := []l.Credential{
-			l.Credential{Name: "private key", Type: "string", Value: privateKey},
-			l.Credential{Name: "public key", Type: "string", Value: publicKey},
-			l.Credential{Name: "password", Type: "string", Value: password},
+		// prepare parameters for the ssh command
+		linePrefix := fmt.Sprintf(
+			"command=\"%s %s %s %s\",no-pty",
+			plugin.PluginInput.ConfigParams["script_path"],
+			plugin.PluginInput.WaTTSUserID,
+			plugin.PluginInput.ConfigParams["myproxy_server"],
+			plugin.PluginInput.ConfigParams["myproxy_server_pwd"])
+
+		/* the key needs to be suffixed with:
+		*	a indicator of the inserting instance and the uid
+		 */
+		instance := plugin.PluginInput.ConfigParams["prefix"].(string)
+		suffixedPublicKey := publicKey + " " + instance + "_" + plugin.PluginInput.WaTTSUserID
+		parameter := map[string]interface{}{
+			"action":       "request",
+			"watts_userid": plugin.PluginInput.WaTTSUserID,
+			"cred_state":   plugin.PluginInput.CredentialState,
+			"params": map[string]string{
+				"key_prefix": linePrefix,
+				"pub_key":    suffixedPublicKey,
+				"state":      plugin.PluginInput.CredentialState,
+			},
 		}
-		credentialState := "registerred"
-		return l.PluginGoodRequest(credential, credentialState)
+		parameterBytes, err := json.Marshal(parameter)
+		l.Check(err, 1, "marshaling script json")
+		encodedScriptParameter := base64url.Encode(parameterBytes)
+		sshTarget := fmt.Sprintf(
+			"%s@%s",
+			plugin.PluginInput.ConfigParams["user"],
+			plugin.PluginInput.ConfigParams["host"])
+
+		// execute the ssh command
+		cmd := exec.Command(
+			"ssh",
+			sshTarget,
+			plugin.PluginInput.ConfigParams["remote_script"].(string),
+			encodedScriptParameter)
+		var out bytes.Buffer
+		cmd.Stdout = &out
+		err = cmd.Run()
+		l.Check(err, 1, "executing ssh command")
+
+		// checking the output of the ssh command
+		var sshCmdOutput map[string]string
+		err = json.Unmarshal(out.Bytes(), &sshCmdOutput)
+		l.Check(err, 1, "unmarshaling output of ssh command")
+
+		if result, ok := sshCmdOutput["result"]; ok {
+			if result == "ok" {
+				// format credentials
+				credential := []l.Credential{
+					l.Credential{Name: "private key", Type: "string", Value: privateKey},
+					l.Credential{Name: "public key", Type: "string", Value: publicKey},
+					l.Credential{Name: "password", Type: "string", Value: password},
+				}
+				credentialState := "registerred"
+				return l.PluginGoodRequest(credential, credentialState)
+			}
+			return l.PluginError(sshCmdOutput["log_msg"])
+		}
+		return l.PluginError("ssh command failed")
 	}
 }
 
@@ -91,7 +152,7 @@ func main() {
 			l.ConfigParamsDescriptor{Name: "remote_script", Type: "string", Default: "./myproxy_ssh_vm.py"},
 			l.ConfigParamsDescriptor{Name: "host", Type: "string", Default: "watts-x509.data.kit.edu"},
 			l.ConfigParamsDescriptor{Name: "user", Type: "string", Default: "x509"},
-			l.ConfigParamsDescriptor{Name: "work_dir", Type: "string", Default: "/tmp"},
+			l.ConfigParamsDescriptor{Name: "prefix", Type: "string", Default: "foobar"},
 		},
 		RequestParams: []l.RequestParamsDescriptor{
 			l.RequestParamsDescriptor{
