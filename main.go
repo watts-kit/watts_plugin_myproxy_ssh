2cd6bb3ab82ff0b242f4e31b1566cc93d4e8f515
Lukas Burgey <ubedv@student.kit.edu>
2017-11-15 14:28:24 +0100

    Check if the public key is already in use

--- main.go
+++ main.go
@@ -1,123 +1,59 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	l "git.scc.kit.edu/lukasburgey/wattsPluginLib"
-	"github.com/kalaspuffar/base64url"
-	"os/exec"
-	"strings"
+)
+
+const (
+	authorizedKeyFile = "~/.ssh/authorized_keys"
 )
 
 func request(pi l.Input) l.Output {
+	h := pi.SSHHostFromConf("user", "host")
 
-	credential := []l.Credential{}
-	var publicKey string
+	publicKey := pi.PublicKeyFromParams("pub_key")
 
-	// if the user has provided a public key we use it instead of generating a key pair
-	if pk, ok := pi.Params["pub_key"]; ok {
-		rawPublicKey, ok := pk.(string)
-		l.CheckOk(ok, 1, "pub_key is no string")
-
-		// parse the public key
-		keyElements := strings.Split(rawPublicKey, " ")
-		switch len(keyElements) {
-		case 2:
-			// case key-type + key
-			publicKey = rawPublicKey
-		case 3:
-			// case key-type + key + comment
-			publicKey = fmt.Sprintf("%s %s", keyElements[0], keyElements[1])
-		default:
-			l.PluginUserError(
-				fmt.Sprintf("Cannot parse user provided public key (e = %v)", len(keyElements)),
-				"Unable to parse the provided ssh public key",
-			)
-		}
+	output, err := h.RunSSHCommandErr(
+		"grep",
+		fmt.Sprintf("'%s'", publicKey),
+		authorizedKeyFile)
+	if output != "" {
+		l.PluginUserError(
+			fmt.Sprint("err:", err, "output:", output),
+			"This public key is already used",)
 	}
 
-	credential = append(
-		credential,
-		l.AutoCredential("retrieval host", fmt.Sprintf("%s@%s",
-			pi.Conf["user"],
-			pi.Conf["host"])))
+	h.RunSSHCommand("cp", authorizedKeyFile, authorizedKeyFile+".bak")
 
-	// prepare parameters for the ssh command
-	sshComment := fmt.Sprintf("%s_%s", pi.Conf["prefix"], pi.WaTTSUserID)
-	linePrefix := fmt.Sprintf("command=\"%s %s %s %s\",no-pty", pi.Conf["script_path"],
-		sshComment, pi.Conf["myproxy_server"], pi.Conf["myproxy_server_pwd"])
 
-	suffixedPublicKey := fmt.Sprintf("%s %s", publicKey, sshComment)
-	pi.Params = map[string]interface{}{
-		"key_prefix": linePrefix,
-		"pub_key":    suffixedPublicKey,
-		"state":      pi.CredentialState,
+	uid := fmt.Sprintf("%s_%s", pi.Conf["prefix"], pi.WaTTSUserID)
+	newLine := fmt.Sprintf("command=\\\"%s %s %s %s\\\",no-pty %s",
+		pi.Conf["script_path"],
+		uid,
+		pi.Conf["myproxy_server"],
+		pi.Conf["myproxy_server_pwd"],
+		publicKey)
+
+	h.RunSSHCommand("echo", newLine, ">>", authorizedKeyFile)
+
+	credentials := []l.Credential{
+		l.AutoCredential("retrieval host", fmt.Sprintf("%s@%s", pi.Conf["user"], pi.Conf["host"])),
 	}
-	parameterBytes, err := json.Marshal(pi)
-	l.Check(err, 1, "marshaling remote script parameter")
 
-	encodedScriptParameter := base64url.Encode(parameterBytes)
-	sshTarget := fmt.Sprintf("%s@%s", pi.Conf["user"], pi.Conf["host"])
-
-	// execute the ssh command
-	remoteScript, ok := pi.Conf["remote_script"].(string)
-	l.CheckOk(ok, 1, "Conf remote_script is no string")
-
-	cmd := exec.Command("ssh", sshTarget, remoteScript, encodedScriptParameter)
-
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err = cmd.Run()
-	l.Check(err, 1, "executing ssh command")
-
-	// checking the output of the ssh command
-	var sshCmdOutput map[string]string
-	err = json.Unmarshal(out.Bytes(), &sshCmdOutput)
-	l.Check(err, 1, "unmarshaling output of ssh command")
-
-	if result, ok := sshCmdOutput["result"]; ok && result == "ok" {
-		return l.PluginGoodRequest(credential, sshComment)
-	}
-	if logMsg, ok := sshCmdOutput["log_msg"]; ok {
-		return l.PluginError(logMsg)
-	}
-	return l.PluginError("request failed")
+	// uid as state
+	return l.PluginGoodRequest(credentials, uid)
 }
 
 func revoke(pi l.Input) l.Output {
-	parameterBytes, err := json.Marshal(pi)
-	l.Check(err, 1, "marshaling parameter for remote script")
-
-	encodedScriptParameter := base64url.Encode(parameterBytes)
-	sshTarget := fmt.Sprintf("%s@%s", pi.Conf["user"], pi.Conf["host"])
-
-	// execute the ssh command
-	remoteScript, ok := pi.Conf["remote_script"].(string)
-	l.CheckOk(ok, 1, "Conf remote_script is no string")
-	cmd := exec.Command("ssh", sshTarget, remoteScript, encodedScriptParameter)
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err = cmd.Run()
-	l.Check(err, 1, "executing ssh command")
-
-	// checking the output of the ssh command
-	var sshCmdOutput map[string]string
-	err = json.Unmarshal(out.Bytes(), &sshCmdOutput)
-	l.Check(err, 1, "unmarshaling output of remote script")
-
-	if result, ok := sshCmdOutput["result"]; ok && result == "ok" {
-		return l.PluginGoodRevoke()
-	}
-	if logMsg, ok := sshCmdOutput["log_msg"]; ok {
-		return l.PluginError(logMsg)
-	}
-	return l.PluginError("revocation failed")
+	h := pi.SSHHostFromConf("user", "host")
+	h.RunSSHCommand("sed", "-i.bak", "/"+pi.CredentialState+"/d", authorizedKeyFile)
+	return l.PluginGoodRevoke()
 }
 
 func main() {
 	pluginDescriptor := l.PluginDescriptor{
-		Version: "0.1.0",
+		Version: "1.0.0",
 		Author:  "Lukas Burgey @ KIT within the INDIGO DataCloud Project",
 		Actions: map[string]l.Action{
 			"request": request,
@@ -126,8 +62,7 @@ func main() {
 		ConfigParams: []l.ConfigParamsDescriptor{
 			l.ConfigParamsDescriptor{Name: "myproxy_server", Type: "string", Default: "master.data.kit.edu"},
 			l.ConfigParamsDescriptor{Name: "myproxy_server_pwd", Type: "string", Default: ""},
-			l.ConfigParamsDescriptor{Name: "script_path", Type: "string", Default: "./myproxy_get_cert.py"},
-			l.ConfigParamsDescriptor{Name: "remote_script", Type: "string", Default: "./myproxy_ssh_vm.py"},
+			l.ConfigParamsDescriptor{Name: "script_path", Type: "string", Default: "./getCert"},
 			l.ConfigParamsDescriptor{Name: "host", Type: "string", Default: "watts-x509.data.kit.edu"},
 			l.ConfigParamsDescriptor{Name: "user", Type: "string", Default: "x509"},
 			l.ConfigParamsDescriptor{Name: "prefix", Type: "string", Default: "foobar"},
