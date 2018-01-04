c582dd9b840534cc802c0de380dedb25612851c6
Marcus <marcus@hardt-it.de>
2018-01-04 14:11:41 +0100

    add support/requirement for passwordc

--- remote/getCert/getCert.go
+++ remote/getCert/getCert.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+    "github.com/watts-kit/passwordd/passwordclib"
 )
 
 const version = "0.1.0"
@@ -17,14 +18,18 @@ func main() {
 	)
 	wattsUID := app.Arg("WaTTS User ID", "").Required().String()
 	host := app.Arg("Host", "").Required().String()
-	password := app.Arg("Password", "").Required().String()
 	app.Author("Lukas Burgey")
 	app.Version(version)
 
+    password, pwdc_err := passwordclib.GetPassword("myproxy_server_pwd")
+    if pwdc_err != nil {
+        fmt.Printf ("error. You should add 'myproxy_server_pwd' to passwordd, for example with ----passwordc set myproxy_server_pwd-------- %s", pwdc_err)
+        panic(pwdc_err)
+    }
 	kingpin.MustParse(app.Parse(os.Args[1:]))
 
 	cmd := exec.Command("myproxy-logon", "-l", *wattsUID, "-s", *host, "-S", "-o", "-")
-	cmd.Stdin = strings.NewReader(*password)
+	cmd.Stdin = strings.NewReader(password)
 
 	output, err := cmd.CombinedOutput()
 	kingpin.FatalIfError(err, string(output))
